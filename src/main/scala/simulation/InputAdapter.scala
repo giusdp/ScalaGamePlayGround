d0d5be2e83eec0bde5bc79b9318a114fafb66512
giusdp <depalma.gsp@gmail.com>
2019-05-26 19:08:59 +0200

    releasedA now updates the state machine with GLFW_RELEASE

--- src/main/scala/simulation/InputAdapter.scala
+++ src/main/scala/simulation/InputAdapter.scala
@@ -1,10 +1,10 @@
 package simulation
 
-import org.lwjgl.glfw.GLFW.GLFW_KEY_A
+import org.lwjgl.glfw.GLFW.{GLFW_KEY_A, GLFW_RELEASE}
 
 object InputAdapter {
 
   def pressedA(): Unit = MovementStateMachine.update(GLFW_KEY_A)
-  def releasedA(): Unit = println("A released")
+  def releasedA(): Unit = MovementStateMachine.update(GLFW_RELEASE)
 
 }
