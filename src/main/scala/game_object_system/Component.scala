cad8cbf0c9c06f5f054cdbd6ff945e6d7ecbfcd0
giusdp <depalma.gsp@gmail.com>
2019-05-28 10:56:54 +0200

    movable com has the state machine

--- src/main/scala/game_object_system/Component.scala
+++ src/main/scala/game_object_system/Component.scala
@@ -6,6 +6,8 @@ sealed trait Component
 
 final case class PositionCom(var x: Int, var y: Int) extends Component
 
-final case class InputCom() extends Component{
+final case class MovableCom(var vel: Int) extends Component {
   var state_machine = new MovementStateMachine()
 }
+
+//final case class MovableCom(var velocity: Int) extends Component
