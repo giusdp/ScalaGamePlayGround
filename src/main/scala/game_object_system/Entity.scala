a47600a81c1d1b5a9f2efe688cf888f6d669e97f
giusdp <depalma.gsp@gmail.com>
2019-05-28 12:37:00 +0200

    Changed name of EntityManager and entity now is only an int ID

--- src/main/scala/game_object_system/Entity.scala
+++ src/main/scala/game_object_system/Entity.scala
@@ -1,27 +1,10 @@
 package game_object_system
 
-class Entity(val ID : Int) {
-
-//  var components : List[Component] = List()
-//
-//  def addComponent(c : Component): Unit = components = c :: components
-
-  override def equals(obj: Any): Boolean = obj match {
-    case Entity => obj.asInstanceOf[Entity].ID == ID
-    case _ => false
-  }
-
-}
-
 object Entity {
-  var lastID = 0
-  def genID(): Int = {
+  private var lastID = 0
+  def spawnEntity(): Int = {
     lastID += 1
     lastID
-  }
-
-  def apply(): Entity = {
-    new Entity(genID())
   }
 
 }
---
+++ src/main/scala/simulation/Simulation.scala
@@ -0,0 +1,11 @@
+package simulation
+
+import game_object_system.{MovableCom, EntityManager}
+
+object World {
+
+//  def update(): Unit = {
+//    EntityManager.getAllOfThisComponents[MovableCom].foreach(c => c.state_machine.currentState)
+//  }
+
+}
