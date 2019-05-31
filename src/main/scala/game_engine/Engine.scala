43ccc49068e87aa60d1de20c6c9c6df9e7b2c953
giusdp <depalma.gsp@gmail.com>
2019-05-31 18:13:05 +0200

    added code to start rendering something

--- src/main/scala/game_engine/Engine.scala
+++ src/main/scala/game_engine/Engine.scala
@@ -1,11 +1,16 @@
 package game_engine
 
+import java.nio._
+
 import datamanager.EntityLoader
 import game_engine.graphics.Renderer
 import org.lwjgl.glfw.Callbacks._
 import org.lwjgl.glfw.GLFW._
-import org.lwjgl.glfw.GLFWErrorCallback
-import org.lwjgl.system.MemoryUtil.NULL
+import org.lwjgl.glfw._
+import org.lwjgl.opengl.GL
+import org.lwjgl.system.MemoryStack._
+import org.lwjgl.system.MemoryUtil._
+import org.lwjgl.system._
 import simulation.Simulation
 
 import scala.annotation.tailrec
@@ -24,6 +29,35 @@ object Engine {
     val window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL)
     if (window == NULL) throw new RuntimeException("Failed to create the GLFW window")
 
+    try {
+
+      val stack: MemoryStack = stackPush()
+      val pWidth: IntBuffer = stack.mallocInt(1); // int*
+      val pHeight: IntBuffer = stack.mallocInt(1); // int*
+
+      // Get the window size passed to glfwCreateWindow
+      glfwGetWindowSize(window, pWidth, pHeight)
+
+      // Get the resolution of the primary monitor
+      val vidmode: GLFWVidMode = glfwGetVideoMode(glfwGetPrimaryMonitor())
+
+      // Center the window
+      glfwSetWindowPos(window, (vidmode.width() - pWidth.get(0)) / 2, (vidmode.height() - pHeight.get(0)) / 2)
+
+      stackPop()
+    }
+    catch  {
+      case e : Exception => println("Error handling window properties when starting engine: " + e.getMessage)
+    }
+
+    // Make the OpenGL context current
+    glfwMakeContextCurrent(window)
+    // Enable v-sync
+    glfwSwapInterval(1)
+
+    // Make the window visible
+    glfwShowWindow(window)
+
     EntityLoader.createEntitiesFromJSON("player.json")
     Input.registerInput(window)
 
@@ -40,12 +74,12 @@ object Engine {
 
   @tailrec
   def game_loop(window: Long): Unit = {
-
+    GL.createCapabilities
     Input.tickInput()
 
     Simulation.update()
 
-    Renderer.renderFrame()
+    Renderer.renderFrame(window)
 
     if (! glfwWindowShouldClose(window))
       game_loop(window)
--- src/main/scala/game_engine/graphics/Renderer.scala
+++ src/main/scala/game_engine/graphics/Renderer.scala
@@ -1,11 +1,15 @@
 package game_engine.graphics
-
-import game_object_system.{ECHandler, Entity, PositionCom, GraphicsCom}
+import game_object_system.{ECHandler, Entity, PositionCom}
+import org.lwjgl.glfw.GLFW._
+import org.lwjgl.opengl.GL11._
 
 object Renderer {
 
-  def renderFrame() = {
-//    ECHandler.renderableEntities.foreach(e => ECHandler.getThisComponentOfE[RenderableCom])
+  def renderFrame(window : Long) = {
+    glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+    glfwSwapBuffers(window); // swap the color buffers
   }
 
   def renderEntity(e: Entity) = {
