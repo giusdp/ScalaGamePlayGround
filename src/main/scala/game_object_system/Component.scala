45edd7bc5cde9597388055a90da334d15c37b55f
giusdp <depalma.gsp@gmail.com>
2019-06-03 09:57:39 +0200

    Added ordered trait to sort components

--- src/main/scala/game_object_system/Component.scala
+++ src/main/scala/game_object_system/Component.scala
@@ -2,20 +2,31 @@ package game_object_system
 
 import simulation.MovementStateMachine
 
-trait Component
+// Ordered is used to order components based on the number of other components required
+trait Component extends Ordered[Component]
 
-case class EmptyCom() extends Component
+case class EmptyCom() extends Component {
+  override def compare(that: Component): Int = 0
+}
 
 case class PositionCom(var x: Float, var y: Float) extends Component{
   def addToX(nx : Float): Unit = x += nx
   def addToY(ny : Float): Unit = y += ny
+
+  override def compare(that: Component): Int = 0 // needs 0 other components
 }
 
 case class MovableCom(var velX: Float, velY: Float) extends Component {
   var state_machine = new MovementStateMachine()
   def movePos(pos: PositionCom): Unit = state_machine.updatePos(pos, (velX, velY))
+
+  override def compare(that: Component): Int = 1
 }
 
-case class InputCom() extends Component
+case class InputCom() extends Component {
+  override def compare(that: Component): Int = 1
+}
 
-case class RenderableCom() extends Component
+case class RenderableCom() extends Component {
+  override def compare(that: Component): Int = 2
+}
