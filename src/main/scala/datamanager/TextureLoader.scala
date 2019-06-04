ab6f9bedee8d718e1f81c7ffd9c28b4377d44283
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-06-04 18:04:26 +0200

    added error handling when loading a texture

--- src/main/scala/datamanager/TextureLoader.scala
+++ src/main/scala/datamanager/TextureLoader.scala
@@ -10,42 +10,47 @@ import org.lwjgl.opengl.{GL11, GL30}
 
 object TextureLoader {
 
-  def loadTexture(fileName: String) : Texture = {
+  def loadTexture(fileName: String) : Option[Texture] = {
 
-    val byteArray = Files.readAllBytes(Paths.get(fileName))
-    //load png file
-    val decoder : PNGDecoder = new PNGDecoder(new ByteArrayInputStream(byteArray))
+    try{
+      val byteArray = Files.readAllBytes(Paths.get(fileName))
+      //load png file
+      val decoder : PNGDecoder = new PNGDecoder(new ByteArrayInputStream(byteArray))
 
-    //create a byte buffer big enough to store RGBA values
-    val buffer = ByteBuffer.allocateDirect(4 * decoder.getWidth * decoder.getHeight)
+      //create a byte buffer big enough to store RGBA values
+      val buffer = ByteBuffer.allocateDirect(4 * decoder.getWidth * decoder.getHeight)
 
-    //decode
-    decoder.decode(buffer, decoder.getWidth * 4, PNGDecoder.Format.RGBA)
+      //decode
+      decoder.decode(buffer, decoder.getWidth * 4, PNGDecoder.Format.RGBA)
 
-    //flip the buffer so its ready to read
-    buffer.flip()
+      //flip the buffer so its ready to read
+      buffer.flip()
 
-    //create a texture
-    val id = GL11.glGenTextures()
+      //create a texture
+      val id = GL11.glGenTextures()
 
-    //bind the texture
-    GL11.glBindTexture(GL11.GL_TEXTURE_2D, id)
+      //bind the texture
+      GL11.glBindTexture(GL11.GL_TEXTURE_2D, id)
 
-    //tell opengl how to unpack bytes
-    GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1)
+      //tell opengl how to unpack bytes
+      GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1)
 
-    //set the texture parameters, can be GL_LINEAR or GL_NEAREST
-    GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR)
-    GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR)
+      //set the texture parameters, can be GL_LINEAR or GL_NEAREST
+      GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR)
+      GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR)
 
-    //upload texture
-    GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, decoder.getWidth, decoder.getHeight,
-      0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer)
+      //upload texture
+      GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, decoder.getWidth, decoder.getHeight,
+        0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer)
 
-    // Generate Mip Map
-    GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D)
+      // Generate Mip Map
+      GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D)
 
-    Texture(id)
+      Some(Texture(id))
+    }
+    catch {
+      case e : Exception => Console.err.println("Error while loading texture: " + fileName) ; None
+    }
   }
 
 }
