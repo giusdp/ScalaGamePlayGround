dc648e35e82f8dd6c603b9c628e15af99163f7b1
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-06-04 18:36:14 +0200

    Added tex coordinates

--- src/main/scala/datamanager/ModelLoader.scala
+++ src/main/scala/datamanager/ModelLoader.scala
@@ -14,19 +14,20 @@ object ModelLoader {
     val vao = GL30.glGenVertexArrays()
     GL30.glBindVertexArray(vao)
 
-    val vbos = List(bindVertexBuffer(s.vertices), bindIndicesBuffer(s.indices))
+    val vbos = List(bindIndicesBuffer(s.indices), bindVertexBuffer(s.vertices), bindTexCoordBuffer(s.tex_coords))
 
     GL30.glBindVertexArray(0)
     Model(vao, vbos, s.indices.length)
   }
 
-  private def bindVertexBuffer(data : Array[Float]) = bindAttributeBuffer(VERTEX_LOCATION, data)
+  private def bindVertexBuffer(data : Array[Float]) = bindAttributeBuffer(VERTEX_LOCATION, 3, data)
+  private def bindTexCoordBuffer(data: Array[Float]) = bindAttributeBuffer(TEXTURE_LOCATION, 2, data)
 
-  private def bindAttributeBuffer(index : Int, data : Array[Float]) = {
+  private def bindAttributeBuffer(index : Int, coordSize : Int, data : Array[Float]) = {
     val vbo = GL15.glGenBuffers()
     GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo)
     GL15.glBufferData(GL15.GL_ARRAY_BUFFER, mkFloatBuffer(data), GL15.GL_STATIC_DRAW)
-    GL20.glVertexAttribPointer(index, 3, GL11.GL_FLOAT, false, 0, 0)
+    GL20.glVertexAttribPointer(index, coordSize, GL11.GL_FLOAT, false, 0, 0)
     GL20.glEnableVertexAttribArray(index)
     vbo
   }
--- src/main/scala/game_object_system/graphics_objects/Shape.scala
+++ src/main/scala/game_object_system/graphics_objects/Shape.scala
@@ -1,12 +1,19 @@
 package game_object_system.graphics_objects
 
 trait Shape {
+  val indices : Array[Int] = Array()
+
   val vertices : Array[Float] = Array()
 
-  val indices : Array[Int] = Array()
+  val tex_coords : Array[Float] = Array()
 }
 
 case class Quad() extends Shape {
+  override val indices: Array[Int] = Array(
+    0,1,3,
+    3,1,2
+  )
+
   override val vertices = Array(
     // Left bottom triangle
     -0.5f, 0.5f, 0f,
@@ -15,8 +22,10 @@ case class Quad() extends Shape {
     0.5f, 0.5f, 0f,
   )
 
-  override val indices: Array[Int] = Array(
-    0,1,3,
-    3,1,2
+  override val tex_coords: Array[Float] = Array(
+    0,0,
+    0,1,
+    1,1,
+    1,0
   )
 }
