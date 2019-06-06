be6d492ba7674c1cecec4c08f1ca03f3698ca55b
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-06-06 19:21:07 +0200

    fixed model matrix usage. To be fixed : movement state machine directions

--- src/main/scala/simulation/Simulation.scala
+++ src/main/scala/simulation/Simulation.scala
@@ -1,37 +1,32 @@
 package simulation
 
-import game_object_system.{ECHandler, Entity, MovableCom}
+import game_object_system.{ECHandler, Entity, MovableCom, PositionCom}
 
 
 object Simulation {
 
   def update(): Unit = {
-    ECHandler.movableEntities.foreach(updatePosOfE)
-
+    updateAllPositions()
   }
 
-  def updatePosOfE(e: Entity): Unit = {
-    val m = ECHandler.getMovableCom(e)
-    m match {
+  def updateAllPositions(): Unit = ECHandler.movableEntities.foreach(updatePosOfE)
 
-
+  def updatePosOfE(e: Entity): Unit =
+      ECHandler.getMovableCom(e) match {
       case Some(move) =>
-        if (! move.state_machine.isStill) {
-          val p = ECHandler.getPositionCom(e)
-          p match {
-            case Some(pos) =>
-              pos.addToX(move.state_machine.currentDirection._1 * move.velX)
-              pos.addToY(move.state_machine.currentDirection._2 * move.velY)
-              pos.model_matrix.translate(pos.x, pos.y, 0)
+        ECHandler.getPositionCom(e) match {
+            case Some(pos) => updatePosition(pos, move)
             case None =>
           }
-        }
-    else{
-
-        }
-
       case None =>
     }
+
+
+  def updatePosition(p : PositionCom, m: MovableCom) : Unit = {
+    p.addToX(m.state_machine.currentDirection._1 * m.velX)
+    p.addToY(m.state_machine.currentDirection._2 * m.velY)
+
+    p.model_matrix.identity().translate(p.x, p.y, 0)
   }
 
 }
