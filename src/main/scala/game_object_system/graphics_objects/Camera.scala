87e0634e5626292cc54568c1709fdd0e9fb2fe41
giusdp <depalma.gsp@gmail.com>
2019-06-24 17:37:01 +0200

    imported constants

--- src/main/scala/game_object_system/graphics_objects/Camera.scala
+++ src/main/scala/game_object_system/graphics_objects/Camera.scala
@@ -2,21 +2,18 @@ package game_object_system.graphics_objects
 
 import game_object_system.Constants
 import org.joml.{Matrix4f, Vector3f}
+import game_object_system.Constants.{WIDTH, HEIGHT}
 
 object Camera {
 
-  val position : Vector3f = new Vector3f()
+  val position : Vector3f = new Vector3f() 
 
-  val proj : Matrix4f = new Matrix4f().setOrtho2D(-Constants.WIDTH / 2, Constants.WIDTH / 2,
-    -Constants.HEIGHT / 2, Constants.HEIGHT / 2)
+  val proj : Matrix4f = new Matrix4f().setOrtho2D(-WIDTH / 2, WIDTH / 2,
+    -HEIGHT / 2, HEIGHT / 2)
 
   def setPosition(x : Float, y : Float, z : Float): Unit = position.set(x, y, z)
 
   def addPosition(x : Float, y : Float, z : Float): Unit = position.add(x, y, z)
 
   def viewProjMat: Matrix4f = proj.mulOrthoAffine(new Matrix4f().setTranslation(position), new Matrix4f())
-
-  def centerCamera(x : Float, y : Float, z : Float): Unit =
-    position.set((x*1.5f) - (Constants.WIDTH/2), (y*1.5f) - (Constants.HEIGHT/2), z)
-
 }
