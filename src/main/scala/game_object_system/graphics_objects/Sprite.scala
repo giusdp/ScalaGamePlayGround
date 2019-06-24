7bc7de2863fd5657ff5fb2b3b506b269e00591a8
giusdp <depalma.gsp@gmail.com>
2019-06-24 17:17:43 +0200

    added info in sprite class

--- src/main/scala/game_object_system/graphics_objects/Sprite.scala
+++ src/main/scala/game_object_system/graphics_objects/Sprite.scala
@@ -1,6 +1,10 @@
 package game_object_system.graphics_objects
 
 case class Sprite(model : Model, texture: Texture) {
+
+  val width: Float = texture.w
+  val height: Float = texture.h
+
   def dispose(): Unit = {
     model.dispose()
     texture.dispose()
