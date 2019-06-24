04abad724ada460f86e366021e3f67cea2310e1e
giusdp <depalma.gsp@gmail.com>
2019-06-24 16:34:01 +0200

    simplified some code

--- src/main/scala/simulation/Simulation.scala
+++ src/main/scala/simulation/Simulation.scala
@@ -12,24 +12,21 @@ object Simulation {
 
   def updateAllPositions(): Unit = ECHandler.movableEntities.foreach(updatePosOfE)
 
-  def updatePosOfE(e: Entity): Unit =
-      ECHandler.getMovableCom(e) match {
-      case Some(move) =>
-        ECHandler.getPositionCom(e) match {
-            case Some(pos) =>
-              updatePosition(pos, move)
-              if (ECHandler.hasThisComponent[CameraCenter](e)) Camera.setPosition(pos.x, pos.y)
-              else {}
-            case None =>
-          }
+  def updatePosOfE(e: Entity): Unit = ECHandler.getMovableCom(e) match {
+    case Some(move) => ECHandler.getPositionCom(e) match {
+      case Some(pos) => updatePosition(pos, move, ECHandler.hasThisComponent[CameraCenter](e))
       case None =>
     }
+    case None =>
+  }
 
 
-  def updatePosition(p : PositionCom, m: MovableCom) : Unit = {
+  def updatePosition(p : PositionCom, m: MovableCom, cc : Boolean) : Unit = {
     p.addToX(m.state_machine.direction._1 * m.velX)
     p.addToY(m.state_machine.direction._2 * m.velY)
     p.model_matrix.identity().translate(p.x, p.y, 0)
+    if (cc) Camera.setPosition(p.x, p.y, 0)
+    else {}
   }
 
 }
