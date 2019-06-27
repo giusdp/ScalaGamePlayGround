16893b919ac3826d1f7e24390ef669e661102cc0
giusdp <depalma.gsp@gmail.com>
2019-06-27 18:58:25 +0200

    changed loaders toload Ashley Entities

--- src/main/scala/datamanager/SpriteLoader.scala
+++ src/main/scala/datamanager/SpriteLoader.scala
@@ -4,9 +4,9 @@ import game_object_system.graphics_objects.{Rect, Sprite}
 
 object SpriteLoader {
 
-  def loadSprite(x: Float, y : Float, imgFileName : String): Option[Sprite] =
+  def loadSprite(imgFileName : String): Option[Sprite] =
     TextureLoader.loadTexture(imgFileName) match {
-      case Some(t) =>  Some(Sprite(ModelLoader.loadModel(Rect(x, y, t.w, t.h)), t))
+      case Some(t) =>  Some(Sprite(ModelLoader.loadModel(Rect(0,0, t.w, t.h)), t))
       case None => None
   }
 
