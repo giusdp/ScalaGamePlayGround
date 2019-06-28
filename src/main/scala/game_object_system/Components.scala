f4c50271e0d822a38b7559e241b74325b8e757a3
giusdp <depalma.gsp@gmail.com>
2019-06-28 12:44:59 +0200

    movementcom to velocitycom

--- src/main/scala/game_object_system/Components.scala
+++ src/main/scala/game_object_system/Components.scala
@@ -2,7 +2,6 @@ package game_object_system
 
 import com.badlogic.ashley.core.Component
 import game_object_system.graphics_objects.Sprite
-import simulation.MovementHandler
 
 case class EmptyCom() extends Component
 
@@ -11,9 +10,7 @@ case class PositionCom(var x: Float, var y: Float) extends Component{
   def addToY(ny : Float): Unit = y += ny
 }
 
-case class MovementCom(var velX: Float, var velY: Float) extends Component {
-  val state_machine = new MovementHandler()
-}
+case class VelocityCom(var velX: Float, var velY: Float) extends Component
 
 // Instead of forcing sprites as the objects to use to render, it could be abstracted away so to
 // allow anything to be rendered by accessing it through this component in the renderer.
--- src/main/scala/game_object_system/ECEngine.scala
+++ src/main/scala/game_object_system/ECEngine.scala
@@ -1,5 +1,14 @@
 package game_object_system
 
-class ECEngine {
+import com.badlogic.ashley.core.{ComponentMapper, Engine}
+
+object ECEngine{
+
+  val engine : Engine = new Engine()
+
+  val posMapper : ComponentMapper[PositionCom] = ComponentMapper.getFor(PositionCom.getClass)
+  val moveMapper : ComponentMapper[VelocityCom] = ComponentMapper.getFor(VelocityCom.getClass)
+  val renderableMapper : ComponentMapper[PositionCom] = ComponentMapper.getFor(RenderableCom.getClass)
+  val cameraCenterMapper : ComponentMapper[PositionCom] = ComponentMapper.getFor(CameraCenter.getClass)
 
 }
