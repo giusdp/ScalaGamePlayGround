7e5fac3f3ac205344843425e6f3af2fdf76e4f6a
giusdp <depalma.gsp@gmail.com>
2019-06-28 13:42:57 +0200

    renderer now is a system part of the ecs

--- src/main/scala/game_engine/graphics/RenderingSystem.scala
+++ src/main/scala/game_engine/graphics/RenderingSystem.scala
@@ -1,14 +1,16 @@
 package game_engine.graphics
 import java.nio.FloatBuffer
 
+import com.badlogic.ashley.systems.SortedIteratingSystem
 import game_object_system.graphics_objects.{Camera, Shader}
-import game_object_system.{ECHandler, Entity, PositionCom, RenderableCom}
+import game_object_system.{ECHandler, OldEntity, PositionCom, RenderableCom}
 import org.lwjgl.BufferUtils
 import org.lwjgl.glfw.GLFW._
 import org.lwjgl.opengl.GL11._
 import org.lwjgl.opengl.{GL11, GL13, GL30}
 
-class Renderer(shader : Shader) {
+class Renderer(shader : Shader) extends SortedIteratingSystem {
+
 
   val fb: FloatBuffer = BufferUtils.createFloatBuffer(16)
 
@@ -16,29 +18,27 @@ class Renderer(shader : Shader) {
     glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
     glClear(GL_COLOR_BUFFER_BIT); // clear the framebuffer
 
-    shader.use()
-
-    ECHandler.renderableEntities.foreach(renderEntity)
-
-    shader.stop()
-
     glfwSwapBuffers(window); // swap the color buffers
   }
 
-  def renderEntity(e: Entity): Unit = {
+  
+
+  def renderEntity(e: OldEntity): Unit = {
 
     def render(p: PositionCom, r: RenderableCom): Unit = {
+      shader.use()
       val sprite = r.sprite
       GL30.glBindVertexArray(sprite.model.vao)
       GL13.glActiveTexture(0)
 
       GL11.glBindTexture(GL11.GL_TEXTURE_2D, sprite.texture.id)
 
-      shader.setMVP(Camera.viewProjMat.mulAffine(r.sprite.getModelMatrix).get(fb))
+      shader.setMVP(r.sprite.getModelMatrix.get(fb))
 
       GL11.glDrawElements(GL11.GL_TRIANGLES, sprite.model.vCount, GL11.GL_UNSIGNED_INT, 0)
 
       GL30.glBindVertexArray(0)
+      shader.stop()
     }
 
     ECHandler.getPositionCom(e) match {
