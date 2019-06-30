d93dde93f8bd9eb1587cbb65641966daa8cbb7d2
giusdp <depalma.gsp@gmail.com>
2019-06-30 17:38:04 +0200

    added overloaded sprite loader to take x y of sprite

--- src/main/scala/datamanager/SpriteLoader.scala
+++ src/main/scala/datamanager/SpriteLoader.scala
@@ -8,6 +8,12 @@ object SpriteLoader {
     TextureLoader.loadTexture(imgFileName) match {
       case Some(t) =>  Some(Sprite(ModelLoader.loadModel(Rect(0,0, t.w, t.h)), t))
       case None => None
-  }
+    }
+
+  def loadSprite(x : Int, y : Int, imgFileName : String): Option[Sprite] =
+    TextureLoader.loadTexture(imgFileName) match {
+      case Some(t) =>  Some(Sprite(ModelLoader.loadModel(Rect(x,y, t.w, t.h)), t))
+      case None => None
+    }
 
 }
