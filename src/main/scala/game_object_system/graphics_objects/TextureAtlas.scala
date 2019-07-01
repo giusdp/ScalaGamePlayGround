8fa0ed7c00e853a43d339640be9853f2fe897b0b
giusdp <depalma.gsp@gmail.com>
2019-07-01 18:59:13 +0200

    texture atlas provides function to extract tiles

--- src/main/scala/game_object_system/graphics_objects/TextureAtlas.scala
+++ src/main/scala/game_object_system/graphics_objects/TextureAtlas.scala
@@ -1,25 +1,20 @@
 package game_object_system.graphics_objects
 
-import datamanager.ModelLoader
+case class TextureAtlas(texture : Texture, cellSize : Float, sheetWidth : Float, sheetHeight: Float) {
 
-case class SpriteSheet(texture : Texture, cellSize : Float, sheetWidth : Float, sheetHeight: Float) {
-
-  def singleSprite(index : Int, x : Float, y : Float) = {
+  def makeTile(index : Int) : Tile = {
     val tw : Float = cellSize/sheetWidth
     val th = cellSize/sheetHeight
     val rows = sheetWidth/cellSize
     val tx = (index % rows) / tw
     val ty = (index / rows + 1) * th
-
-    val texCoords = Array(
+    val tc = Array(
       tx, ty,
       tx + tw, ty,
       tx + tw, ty + th,
       tx, ty + th
     )
-
-    val r = CustomTextureRect(x, y, cellSize, cellSize, texCoords)
-    ModelLoader.loadModel(r)
-
+    Tile(CustomTextureRect(0, 0, cellSize, cellSize, tc))
   }
+
 }
