3737f3345cb19b6d2ed2b19ac9da1008f7709bb8
giusdp <depalma.gsp@gmail.com>
2019-07-04 21:46:11 +0200

    added utility fields for width and height

--- src/main/scala/game_object_system/graphics_objects/TextureAtlas.scala
+++ src/main/scala/game_object_system/graphics_objects/TextureAtlas.scala
@@ -3,9 +3,9 @@ package game_object_system.graphics_objects
 case class TextureAtlas(texture : Texture, cellWidth : Float, cellHeight : Float) {
 
   def makeTile(index : Int) : Tile = {
-    val tw : Float = cellWidth/texture.w
-    val th = cellHeight/texture.h
-    val columns = texture.w/cellWidth
+    val tw : Float = cellWidth/imageWidth
+    val th = cellHeight/imageHeight
+    val columns = imageWidth/cellWidth
     val tx = (index % columns) / tw
     val ty = (index / columns + 1) * th
     val tc = Array(
@@ -16,6 +16,9 @@ case class TextureAtlas(texture : Texture, cellWidth : Float, cellHeight : Float
     )
     Tile(tc)
   }
+
+  val imageWidth: Float = texture.w
+  val imageHeight: Float = texture.h
 
   def dispose(): Unit = texture.dispose()
 
