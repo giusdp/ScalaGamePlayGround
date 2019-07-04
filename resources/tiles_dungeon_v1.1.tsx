8bf3e8b98a2741942479b60bd1174ec4246d03e5
giusdp <depalma.gsp@gmail.com>
2019-07-04 18:56:39 +0200

    update

--- resources/tiles_dungeon_v1.1.tsx
+++ resources/tiles_dungeon_v1.1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="2019.06.20" name="tiles_dungeon_v1.1" tilewidth="16" tileheight="16" tilecount="480" columns="20">
- <image source="../Scaricati/rpgdungeontilesetplus2bonuscharacters/Assets/tiles_dungeon_v1.1.png" width="320" height="384"/>
+ <image source="tiles_dungeon.png" width="320" height="384"/>
 </tileset>
