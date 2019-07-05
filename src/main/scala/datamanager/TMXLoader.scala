74093f22f9a6ef8526a0a216f6f5078335a7bbcb
giusdp <depalma.gsp@gmail.com>
2019-07-05 12:59:20 +0200

    TMX loader to load tmx map into TileMap objects

--- src/main/scala/datamanager/TMXLoader.scala
+++ src/main/scala/datamanager/TMXLoader.scala
@@ -2,62 +2,103 @@ package datamanager
 
 import java.awt.Rectangle
 
-import game_object_system.{TMXLayer, TMXMap, TMXObject, TMXObjectGroup, TmxTileset}
+import game_object_system.graphics_objects.{CustomTextureRect, Model, ObjectLayer, ObjectShape, TextureAtlas, TileLayer, TileMap}
 
-import scala.xml.{Node, XML}
+import scala.xml.{Node, NodeSeq, XML}
 
 object TMXLoader {
-  val DATA = "data"
-  val IMAGE = "image"
-  val LAYER = "layer"
-  val OBJECT = "object"
-  val OBJECT_GROUP = "objectgroup"
-  val TILE_SET = "tileset"
-  val TILE = "tile"
-  val FIRST_GID = "@firstgid"
-  val GID = "@gid"
-  val NAME = "@name"
-  val SOURCE = "@source"
-  val Y = "@y"
-  val X = "@x"
-  val WIDTH = "@width"
-  val HEIGHT = "@height"
-  val TILE_WIDTH = "@tilewidth"
-  val TILE_HEIGHT = "@tileheight"
-  val IMAGE_SOURCE = "@image source"
+  private val DATA = "data"
+  private val IMAGE = "image"
+  private val LAYER = "layer"
+  private val OBJECT = "object"
+  private val OBJECT_GROUP = "objectgroup"
+  private val TILE_SET = "tileset"
+  private val TILE = "tile"
+  private val FIRST_GID = "@firstgid"
+  private val GID = "@gid"
+  private val NAME = "@name"
+  private val SOURCE = "@source"
+  private val Y = "@y"
+  private val X = "@x"
+  private val WIDTH = "@width"
+  private val HEIGHT = "@height"
+  private val TILE_WIDTH = "@tilewidth"
+  private val TILE_HEIGHT = "@tileheight"
 
-  def parseTSX(fileName: String): TmxTileset = {
-    val xml = XML.loadFile("/home/giuseppe/Repositories/SGExp/resources/"+fileName)
-    TmxTileset((xml \ NAME).text,getInt((xml \ TILE_WIDTH).text),getInt((xml \ TILE_HEIGHT).text),(xml \ IMAGE_SOURCE).text)
+  private def parseTSX(fileName: String): TextureAtlas = {
+    val xml = XML.loadFile(Resource.RES_DIR+fileName)
+    val imageFileName = (xml \ IMAGE \ SOURCE).text
+    val tw = getInt((xml \ TILE_WIDTH).text)
+    val th = getInt((xml \ TILE_HEIGHT).text)
+    TextureLoader.loadTextureAtlas(imageFileName, tw, th)
   }
 
-  def parseTMX(fileName: String): TMXMap = {
-    val xml = XML.loadFile(fileName)
+  def parseTMX(fileName: String): TileMap = {
+//    println("Parsing " + fileName)
+    val xml = XML.loadFile(Resource.RES_DIR+fileName)
+//    println("XML loaded")
+
     val width: Int = getInt((xml \ WIDTH).text)
     val height: Int = getInt((xml \ HEIGHT).text)
     val tileWidth: Int = getInt((xml \ TILE_WIDTH).text)
     val tileHeight: Int = getInt((xml \ TILE_HEIGHT).text)
 
-    val tileSet: Seq[TmxTileset] = (xml \ TILE_SET) collect {case ts: Node => parseTSX((ts \ SOURCE).text)}
+//    println("Loaded attributes: width " + width + ", height " + height+", tileWidth " + tileWidth+", tileHeight "+tileHeight)
 
-    val layers: Seq[TMXLayer] = (xml \ LAYER) collect { case layer: Node =>
-      TMXLayer((layer \ NAME).text, getInt((layer \ WIDTH).text),getInt((layer \ HEIGHT).text), (layer \ DATA).text)
-    }
+    val tileSet: TextureAtlas = ((xml \ TILE_SET) collectFirst {case ts: Node => parseTSX((ts \ SOURCE).text)}).get
+//    println("TileSet (textureAtlas) loaded: tW " +tileSet.imageWidth + ", tH " + tileSet.imageHeight )
 
-    val objectGroup: Seq[TMXObjectGroup] = (xml \ OBJECT_GROUP) collect { case objectGroup: Node =>
-      val objects: Seq[TMXObject] = objectGroup \ OBJECT collect { case o: Node =>
+    val tileLayers: Seq[TileLayer] = prepareTileLayers(xml \ LAYER, tileWidth, tileHeight, tileSet)
+//    println("Layers loaded: nl " + tileLayers.length + ", numberTiles each layer: " + tileLayers.head.tiles.length)
+
+    val objectLayers: Seq[ObjectLayer] = prepareObjectLayers(xml \ OBJECT_GROUP)
+//    println("Objects (rectangles) loaded: nr " + objectLayers.head.objects.length)
+
+//    println("TileMap loaded")
+    TileMap(width, height, tileWidth, tileHeight, tileSet, tileLayers, objectLayers)
+  }
+
+
+
+
+
+
+
+  private val getInt: String => Int = (s: String) => if (s.nonEmpty) s.toFloat.toInt else -1
+
+  private def prepareTileLayers(layers : NodeSeq, tileWidth : Float, tileHeight: Float, atlas: TextureAtlas) : Seq[TileLayer] = layers collect {
+    case layer: Node =>
+      val layerWidth = getInt((layer \ WIDTH).text)
+      val layerHeight = getInt((layer \ HEIGHT).text)
+      val data: Array[Int] = (layer \ DATA).text.split(",").map(_.trim.toInt)
+
+      val tiles: Seq[Model] = for {
+        y <- 0 until layerHeight
+        x <- 0 until layerWidth
+        if data(x + (y*layerHeight)) != 0
+      } yield buildTile(x*tileWidth, y*tileHeight, tileWidth, tileHeight, data(x + (y*layerHeight)), atlas)
+//        .filterNot(_==0)
+//        .map(t => buildTile(tileWidth, tileHeight, t, atlas))
+
+      TileLayer((layer \ NAME).text, layerWidth, layerHeight, tiles.toArray)
+  }
+
+  private def buildTile(x : Float, y : Float, tw: Float, th: Float, id: Int, atlas: TextureAtlas): Model = {
+    val tcs = atlas.extractRegion(id)
+    ModelLoader.loadModel(CustomTextureRect(x, y, tw, th, tcs))
+  }
+
+  private def prepareObjectLayers(groups : NodeSeq) : Seq[ObjectLayer] = groups collect {
+    case objectGroup: Node =>
+      val objects: Seq[ObjectShape] = objectGroup \ OBJECT collect { case o: Node =>
         val x = getInt((o \ X).text)
         val y = getInt((o \ Y).text)
         val w = getInt((o \ WIDTH).text)
         val h = getInt((o \ HEIGHT).text)
-        TMXObject(getInt((o \ GID).text), new Rectangle(x, y, w, h))
+        ObjectShape(getInt((o \ GID).text), new Rectangle(x, y, w, h))
       }
-      TMXObjectGroup((objectGroup \ NAME).text, getInt((objectGroup \ WIDTH).text),
+      ObjectLayer((objectGroup \ NAME).text, getInt((objectGroup \ WIDTH).text),
         getInt((objectGroup \ HEIGHT).text), objects.toList)
-    }
-
-    TMXMap(width, height, tileWidth, tileHeight, tileSet, layers, objectGroup)
   }
 
-  val getInt: String => Int = (s: String) => if (s.nonEmpty) s.toFloat.toInt else -1
 }
--- src/main/scala/game_object_system/graphics_objects/TileMap.scala
+++ src/main/scala/game_object_system/graphics_objects/TileMap.scala
@@ -1,31 +1,59 @@
-package game_object_system
+package game_object_system.graphics_objects
 
 import java.awt.Rectangle
 
-import game_object_system.graphics_objects.TextureAtlas
+case class TileLayer(name: String, width: Int, height: Int, tiles: Array[Model])
 
-case class TMXTile(gid : Int)
+case class ObjectLayer(name:String, width:Int, height:Int, objects: List[ObjectShape])
+case class ObjectShape(gid:Int, rect:Rectangle)
 
-case class TMXLayer(name: String, width: Int, height: Int, tiles: Array[TMXTile])
-object TMXLayer{
-  def apply(name: String, width: Int, height: Int, data: String): TMXLayer = {
-    val tiles: Array[TMXTile] = data.split(",").map(t => TMXTile(t.trim.toInt))
-    new TMXLayer(name, width, height, tiles)
-  }
-}
-
-case class TMXObject(gid:Int, rect:Rectangle)
-case class TMXObjectGroup(name:String, width:Int, height:Int, objects: List[TMXObject])
-
-case class TMXTileSet(name:String, tileWidth:Int, tileHeight:Int, tsImage: TextureAtlas)
+case class TileMap(
+                    width : Int,
+                    height : Int,
+                    tileWidth: Int,
+                    tileHeight: Int,
+                    tileSet: TextureAtlas,
+                    tileLayers: Seq[TileLayer],
+                    objectGroup: Seq[ObjectLayer]
+                  )
+//  var mapLayers : Seq[Model] = _
 
 
-case class TMXMap(
-                   width : Int,
-                   height : Int,
-                   tileWidth: Int,
-                   tileHeight: Int,
-                   tileSet: TMXTileSet,
-                   layers: Seq[TMXLayer],
-                   objectGroup: Seq[TMXObjectGroup]
-                 )
+
+//object TileMap {
+//  def apply(width: Int, height: Int, tileWidth: Int, tileHeight: Int, tileSet: TextureAtlas, tileLayers: Seq[TileLayer],
+//            objectGroup: Seq[ObjectLayer]): TileMap = {
+//
+//    val map = new TileMap(width, height, tileWidth, tileHeight, tileSet, tileLayers, objectGroup)
+//
+//    var indices : List[Array[Int]] = List()
+//    var vertices : List[Array[Float]] = List()
+//    var texCoords : List[Array[Float]] = List()
+//
+//    map.mapLayers = tileLayers.map(l => {
+//      val inds : Array[Int] = prepareIndices(l.tiles)
+//      for (y <- 0 until l.height) for (x <- 0 until l.width){
+//        if (l.tiles(y * l.height + x).gid != 0) {
+//          val vs : Array[Float] = Array(
+//            x*tileWidth-(tileWidth/2), y*tileHeight+(tileHeight/2), 0,
+//            x*tileWidth-(tileWidth/2), y*tileHeight-(tileHeight/2), 0,
+//            x*tileWidth+(tileWidth/2), y*tileHeight-(tileHeight/2), 0,
+//            x*tileWidth+(tileWidth/2), y*tileHeight+(tileHeight/2), 0
+//          )
+//          val tcs = tileSet.extractRegion(l.tiles(y * l.height + x).gid)
+//          indices = inds :: indices
+//          vertices = vs :: vertices
+//          texCoords = tcs :: texCoords
+//        }
+//      }
+//      ModelLoader.loadModel(indices.flatten.toArray, vertices.flatten.toArray, texCoords.flatten.toArray)
+//    })
+//
+//    map
+//  }
+//
+//  def prepareIndices(tiles : Array[Tile]) : Array[Int] = {
+//    val baseInds: Array[Int] = Array(0,1,3, 3,1,2)
+//    tiles.filterNot(_.gid==0).indices.flatMap(i => baseInds.map(_+(i*4))).toArray
+//  }
+//}
