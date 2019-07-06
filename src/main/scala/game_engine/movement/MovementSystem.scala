6286ae91922b88faa72fce151615d35adde060b3
giusdp <depalma.gsp@gmail.com>
2019-07-06 20:40:43 +0200

    camera center works

--- src/main/scala/game_engine/movement/MovementSystem.scala
+++ src/main/scala/game_engine/movement/MovementSystem.scala
@@ -2,7 +2,9 @@ package game_engine.movement
 
 import com.badlogic.ashley.core.{Entity, Family}
 import com.badlogic.ashley.systems.IteratingSystem
+import game_object_system.graphics_objects.Camera
 import game_object_system.{DirectionCom, ECEngine, PositionCom, VelocityCom}
+import org.joml.Vector3f
 
 class MovementSystem(priority : Int) extends IteratingSystem(
   Family.all(classOf[PositionCom], classOf[VelocityCom], classOf[DirectionCom]).get(),
@@ -19,6 +21,9 @@ class MovementSystem(priority : Int) extends IteratingSystem(
 
     if (ECEngine.renderableMapper.has(entity)) {
       ECEngine.renderableMapper.get(entity).sprite.moveSprite(p.x, p.y, p.z)
+    }
+    if (ECEngine.cameraCenterMapper.has(entity)) {
+      Camera.setPosition(p.x, p.y)
     }
   }
 }
