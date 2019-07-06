a143d280d806ae6be3b5ea2d2c01c0800f568eb8
giusdp <depalma.gsp@gmail.com>
2019-07-06 20:40:09 +0200

    fixed camera

--- src/main/scala/game_object_system/graphics_objects/Camera.scala
+++ src/main/scala/game_object_system/graphics_objects/Camera.scala
@@ -4,26 +4,25 @@ import org.joml.{Matrix4f, Vector3f}
 
 object Camera {
 
-  val position : Vector3f = new Vector3f(0,0,0)
-  var scaleFactor = 1f
+  var aspectRatio : Float = _
+
+  val position : Vector3f = new Vector3f(0,0,10)
+  var scaleFactor :Float = 2f
   var proj : Matrix4f = _
 
-  def setViewSize(w : Float, h: Float): Unit = {
-    val aspectRatio = w/h
-    val viewSize : Float = 900
-    proj = new Matrix4f().setOrtho2D(-aspectRatio*viewSize / 2, aspectRatio*viewSize / 2,
-      -viewSize / 2, viewSize / 2)
-  }
-  def setPosition(x : Float, y : Float, z : Float): Unit = position.set(x, y, z)
+  def setViewSize(w : Float, h: Float): Unit = proj = new Matrix4f().setOrtho(-w/2, w/2, -h/2, h/2, -100, 100)
 
-  def move(x : Float, y : Float, z : Float): Unit = position.add(x, y, z)
-
-  def projection(): Matrix4f = {
-    val pos = new Matrix4f().setTranslation(position).scale(scaleFactor)
-    proj.mul(pos, new Matrix4f())
+  def setPosition(x : Float, y : Float): Unit = {
+    position.x = -x * scaleFactor
+    position.y = -y * scaleFactor
   }
 
-  def zoomIn(): Unit = scaleFactor += 0.1f
-  def zoomOut(): Unit = scaleFactor -= 0.1f
+  def getProjection: Matrix4f = {
+    val view = new Matrix4f().setTranslation(position).scale(scaleFactor)
+    proj.mul(view, new Matrix4f())
+  }
+
+  def zoomIn(): Unit = scaleFactor += 1f
+  def zoomOut(): Unit = if (scaleFactor > 0) scaleFactor -= 1f
 
 }
