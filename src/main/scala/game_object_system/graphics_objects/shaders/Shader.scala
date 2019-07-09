937aeb885adf8ebdd6d227780dad9983de6cfc0f
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-09 15:39:55 +0200

    Adding new types of shaders: TileMapShader

--- src/main/scala/game_object_system/graphics_objects/shaders/Shader.scala
+++ src/main/scala/game_object_system/graphics_objects/shaders/Shader.scala
@@ -4,22 +4,22 @@ import java.nio.FloatBuffer
 
 import org.lwjgl.opengl.GL20
 
-class Shader(program : Int) {
+case class Shader(program : Int) {
 
   val mvpLocation: Int = GL20.glGetUniformLocation(program, "mvp")
 
-  def dispose(): Unit = {
-    GL20.glDeleteProgram(program)
-  }
+  def loadMVP(fb : FloatBuffer): Unit = GL20.glUniformMatrix4fv(mvpLocation, false, fb)
 
   def use(): Unit = GL20.glUseProgram(program)
 
   def stop(): Unit = GL20.glUseProgram(0)
 
-  def setMVP(fb : FloatBuffer): Unit = GL20.glUniformMatrix4fv(mvpLocation, false, fb)
+  def dispose(): Unit = {
+    GL20.glDeleteProgram(program)
+  }
 
 }
 
-object Shader {
-  def apply(program: Int): Shader = new Shader(program)
+class TileMapShader(program : Int) extends Shader(program) {
+
 }
