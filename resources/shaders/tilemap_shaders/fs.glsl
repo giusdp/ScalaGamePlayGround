339d51f9696ac027cd34cb89fa7870bb8f6500df
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-10 16:27:04 +0200

    working on shaders to use instancing

--- resources/shaders/tilemap_shaders/fs.glsl
+++ resources/shaders/tilemap_shaders/fs.glsl
@@ -1,7 +1,20 @@
 #version 330 core
 
 in vec3 final_color;
-out vec4 out_Color;
+out vec4 fragment_color;
+
+uniform sampler2D sampler;
+
 void main(void){
-    out_Color = vec4(final_color, 1.0);
+    fragment_color = vec4(final_color, 1.0);
+//    fragment_color = texture(sampler, tex_coords);
 }
+
+//in vec2 tex_coords;
+//out vec4 out_Color;
+//
+//uniform sampler2D sampler;
+//
+//void main(void){
+//    out_Color = texture(sampler, tex_coords);
+//}
--- resources/shaders/tilemap_shaders/vs.glsl
+++ resources/shaders/tilemap_shaders/vs.glsl
@@ -1,11 +1,15 @@
 #version 330 core
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec2 tex;
+layout (location = 2) in vec3 offset;
 
-out vec3 color;
+uniform mat4 mvp;
+
+out vec3 final_color;
+//out vec2 tex_coords;
 
 void main(void){
-    gl_Position = vec4(pos, 1.0);
-    color = vec3(1,1, pos.z);
+    gl_Position = mvp * vec4(pos + offset, 1.0);
+    final_color = vec3(pos.x + offset.x, pos.y + offset.y, 0.5);
+//    tex_coords = tex;
 }
-
