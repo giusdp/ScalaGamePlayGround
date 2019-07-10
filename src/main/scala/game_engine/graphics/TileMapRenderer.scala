b3d40ea6e29ae7eb6486263fbd6f03ff3535edb3
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-10 16:28:07 +0200

    tilemap renderer now uses instancing

--- src/main/scala/game_engine/graphics/TileMapRenderer.scala
+++ src/main/scala/game_engine/graphics/TileMapRenderer.scala
@@ -8,6 +8,7 @@ import datamanager.ModelLoader
 import game_object_system.graphics_objects.{Camera, Model}
 import game_object_system.graphics_objects.shaders.TileMapShader
 import game_object_system.{ECEngine, TileMapCom}
+import org.joml.Matrix4f
 import org.lwjgl.BufferUtils
 import org.lwjgl.opengl.{GL11, GL31}
 
@@ -17,15 +18,7 @@ class TileMapRenderer(shader: TileMapShader, priority : Int) extends IteratingSy
   Family.all(classOf[TileMapCom]).get(), priority){
 
   val fb: FloatBuffer = BufferUtils.createFloatBuffer(16)
-  val vertices: Array[Float] = Array(
-    -0.05f,  0.05f,0f,
-    0.05f, -0.05f,  0f,
-    -0.05f, -0.05f,0f,
 
-    -0.05f,  0.05f,0f,
-    0.05f, -0.05f, 0f,
-    0.05f,  0.05f,0f,
-  )
   val quad: Model = buildTrans()
 
   def buildTrans() : Model = {
@@ -35,35 +28,21 @@ class TileMapRenderer(shader: TileMapShader, priority : Int) extends IteratingSy
       ts.addOne(y/10.0f)
       ts.addOne(0)
     }
-    ModelLoader.loadTileMapModel(vertices, ts.toArray)
+    ModelLoader.loadTileMapModel(ts.toArray)
   }
 
   override def processEntity(entity: Entity, deltaTime: Float): Unit = {
     val map = ECEngine.tileMapMapper.get(entity).map
 
     shader.use()
-//    map.pointsArrayModel.bindModel()
-//    map.tileSet.bindTextureAtlas(0)
-    val mvp = Camera.getProjection.get(fb)
-    shader.loadMVP(mvp)
-    quad.bindModel()
-//    GL11.glDrawArrays(GL11.GL_POINTS, 0, map.pointsArrayModel.vCount)
-//    map.pointsArrayModel.unBindModel()
-    GL31.glDrawArraysInstanced(GL11.GL_TRIANGLES,0, 6, 100)
-    quad.unBindModel()
+    map.tileLayers.foreach(l => {
+      l.tmModel.bindModel()
+      val mvp = Camera.getProjection.mulOrthoAffine(l.tmModel.model_matrix.scale(64, new Matrix4f())).get(fb)
+      shader.loadMVP(mvp)
+      GL31.glDrawArraysInstanced(GL11.GL_TRIANGLES,0, 6, l.nTiles)
+      l.tmModel.unBindModel()
+    })
     shader.stop()
-    //
-    //    map.tileLayers.foreach(layer  => {
-    //      layer.tiles.foreach(tile => {
-    //        GL30.glBindVertexArray(tile.vao)
-    //
-    //        shader.loadMVP(Camera.getProjection.mulOrthoAffine(tile.model_matrix).get(fb))
-    //
-    //        GL11.glDrawElements(GL11.GL_TRIANGLES, tile.vCount, GL11.GL_UNSIGNED_INT, 0)
-    //
-    //        GL30.glBindVertexArray(0)
-    //      })
-    //    })
   }
 }
 
