11a2790db457ee7833c857ed0b7f714675118d61
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-13 17:42:25 +0200

    testing instanced rendering with test tmx map

--- resources/shaders/tilemap_shaders/fs.glsl
+++ resources/shaders/tilemap_shaders/fs.glsl
@@ -1,20 +1,26 @@
-#version 330 core
+#version 420 core
 
-in vec3 final_color;
+//in vec3 final_color;
+in vec2 tex_coords;
 out vec4 fragment_color;
 
 uniform sampler2D sampler;
 
 void main(void){
-    fragment_color = vec4(final_color, 1.0);
-//    fragment_color = texture(sampler, tex_coords);
+//    fragment_color = vec4(final_color, 1.0);
+    fragment_color = texture(sampler,tex_coords);
+
 }
 
-//in vec2 tex_coords;
-//out vec4 out_Color;
+
+//uniform sampler2DArray tex;
 //
-//uniform sampler2D sampler;
+//flat in uint fragLayer;
+//in vec2 fragTexCoord;
 //
-//void main(void){
-//    out_Color = texture(sampler, tex_coords);
+//out vec4 colour;
+//
+//void main()
+//{
+//    colour = texture( tex, vec3( fragTexCoord, fragLayer ) );
 //}
--- resources/shaders/tilemap_shaders/vs.glsl
+++ resources/shaders/tilemap_shaders/vs.glsl
@@ -1,15 +1,37 @@
-#version 330 core
+#version 420 core
 layout (location = 0) in vec3 pos;
-layout (location = 1) in vec2 tex;
-layout (location = 2) in vec3 offset;
+layout (location = 1) in vec2 texs;
+layout (location = 2) in vec3 pos_offset;
+layout (location = 3) in vec2 tex_offset;
 
 uniform mat4 mvp;
 
-out vec3 final_color;
-//out vec2 tex_coords;
+out vec2 tex_coords;
 
 void main(void){
-    gl_Position = mvp * vec4(pos + offset, 1.0);
-    final_color = vec3(pos.x + offset.x, pos.y + offset.y, 0.5);
-//    tex_coords = tex;
+    int corner = gl_VertexID % 4;
+    gl_Position = mvp * vec4(pos + pos_offset, 1.0);
+    if (texs.x == 0 && texs.y == 0){
+        tex_coords = tex_offset;
+    }
+    else if (texs.x == 0 && texs.y == 1){
+        tex_coords = vec2(tex_offset.x, tex_offset.y*2);
+    }
+    else if (texs.x == 1 && texs.y == 1){
+        tex_coords = vec2(0.15f, 0.041666668f*2);
+        //        tex_coords = vec2(texs.x + (0.15 - 1), texs.y + (0.083333336 - 1));
+    } else {
+        tex_coords = vec2(0.15f, 0.041666668f);
+    }
 }
+    //    if (corner == 0) {
+    //        tex_coords = vec2(uv.x + 0.1, uv.y + (0.083333336 - 1));
+    //    }
+    //    else if (corner == 1) {
+    //        tex_coords = vec2(uv.x + 0.1 , uv.y + 0.041666668);
+    //    }else if (corner == 2) {
+    //        tex_coords = vec2(uv.x + (0.15 - 1), uv.y + 0.041666668);
+    //    }else if (corner == 3) {
+    //        tex_coords = vec2(uv.x + (0.15 - 1), uv.y + (0.083333336 - 1));
+    //    }
+
