7d9181a9e20bb5e0c1b79b02818aa46fd3754c93
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-13 17:44:23 +0200

    Tile map model loader method with offsets arrays to support instancing

--- src/main/scala/datamanager/ModelLoader.scala
+++ src/main/scala/datamanager/ModelLoader.scala
@@ -9,7 +9,8 @@ import org.lwjgl.opengl.{GL11, GL15, GL20, GL30, GL33}
 object ModelLoader {
   val VERTEX_LOCATION = 0
   val TEXTURE_LOCATION = 1
-  val OFFSETS_LOCATION = 2
+  val VERTEX_OFFSETS_LOCATION = 2
+  val TEXTURE_OFFSETS_LOCATION = 3
 
   def loadModel(s : ModelPrimitive): Model = {
     val vao = GL30.glGenVertexArrays()
@@ -41,36 +42,53 @@ object ModelLoader {
     Model(vao, vbos, vertices.length/3)
   }
 
-  def loadTileMapModel(offsets : Array[Float]): Model = {
+  def loadTileMapModel(vertexOffsets : Array[Float], textureOffsets : Array[Float]): Model = {
     val quad: Array[Float] = Array(
-      -0.05f,  0.05f,0f,
-      0.05f, -0.05f,  0f,
-      -0.05f, -0.05f,0f,
+      -1f, 1f,  0f,
+      -1f, -1f, 0f,
+      1f,  1f,  0f,
 
-      -0.05f,  0.05f,0f,
-      0.05f, -0.05f, 0f,
-      0.05f,  0.05f,0f,
+      1f, -1f,  0f,
+      1f,  1f,  0f,
+      -1f, -1f, 0f,
     )
+    val texs : Array[Float] = Array(
+      0,0,
+      0,1,
+      1,0,
+
+      1,1,
+      1,0,
+      0,1,
+    )
+    println(textureOffsets.toList)
     val vao = GL30.glGenVertexArrays()
     GL30.glBindVertexArray(vao)
-    val vbos = List(bindVertexBuffer(quad), bindOffsetsBuffer(offsets))
+    val vbos = List(bindVertexBuffer(quad), bindTexCoordBuffer(texs), bindVertexOffsetsBuffer(vertexOffsets), bindTextureOffsetsBuffer(textureOffsets))
     GL30.glBindVertexArray(0)
     Model(vao, vbos, quad.length/3)
   }
 
   private def bindVertexBuffer(data : Array[Float]) = bindAttributeBuffer(VERTEX_LOCATION, 3, data)
   private def bindTexCoordBuffer(data: Array[Float]) = bindAttributeBuffer(TEXTURE_LOCATION, 2, data)
-  private def bindOffsetsBuffer(data: Array[Float]) = {
-    val vbo = bindAttributeBuffer(OFFSETS_LOCATION, 3, data)
-    GL33.glVertexAttribDivisor(OFFSETS_LOCATION,1)
+  private def bindVertexOffsetsBuffer(data: Array[Float]) = {
+    val vbo = bindAttributeBuffer(VERTEX_OFFSETS_LOCATION, 3, data)
+    GL33.glVertexAttribDivisor(VERTEX_OFFSETS_LOCATION,1)
+    vbo
+  }
+  private def bindTextureOffsetsBuffer(data: Array[Float]) = {
+    val vbo = bindAttributeBuffer(TEXTURE_OFFSETS_LOCATION, 2, data)
+    GL33.glVertexAttribDivisor(TEXTURE_OFFSETS_LOCATION,1)
     vbo
   }
 
   private def bindAttributeBuffer(index : Int, coordSize : Int, data : Array[Float]) = {
     val vbo = GL15.glGenBuffers()
+    val buffer = mkFloatBuffer(data)
     GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo)
-    GL15.glBufferData(GL15.GL_ARRAY_BUFFER, mkFloatBuffer(data), GL15.GL_STATIC_DRAW)
+    GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW)
     GL20.glVertexAttribPointer(index, coordSize, GL11.GL_FLOAT, false, 0, 0)
+    if (index == TEXTURE_LOCATION || index == TEXTURE_OFFSETS_LOCATION) GL11.glTexCoordPointer(2, GL11.GL_FLOAT, 0,buffer )
     GL20.glEnableVertexAttribArray(index)
     vbo
   }
