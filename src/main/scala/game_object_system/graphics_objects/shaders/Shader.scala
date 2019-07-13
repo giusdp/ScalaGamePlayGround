a24a7f3cf85d04dc97c04dcfc1d217cdadb931ec
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-13 19:21:56 +0200

    texture atlas extract first coordinate of region and with region width and height the other coordinates are calcualted in the shader

--- src/main/scala/game_object_system/graphics_objects/shaders/Shader.scala
+++ src/main/scala/game_object_system/graphics_objects/shaders/Shader.scala
@@ -19,10 +19,16 @@ case class Shader(program : Int){
 }
 
 case class TileMapShader(shader: Shader) {
+  val tileWidthLocation: Int = GL20.glGetUniformLocation(shader.program, "tile_width")
+  val tileHeightLocation: Int = GL20.glGetUniformLocation(shader.program, "tile_height")
+
   def use(): Unit = shader.use()
 
   def stop(): Unit = shader.stop()
 
   def loadMVP(fb: FloatBuffer): Unit = shader.loadMVP(fb)
+
+  def loadTextureTileWidth(tw : Float): Unit = GL20.glUniform1f(tileWidthLocation, tw)
+  def loadTextureTileHeight(th : Float): Unit = GL20.glUniform1f(tileHeightLocation, th)
 
 }
