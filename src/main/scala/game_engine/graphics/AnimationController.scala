f94b82ead54b3f1710262d8e10bb5f60ff61e7fa
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-15 18:35:10 +0200

    added animation component

--- src/main/scala/game_engine/graphics/AnimationController.scala
+++ src/main/scala/game_engine/graphics/AnimationController.scala
@@ -1,5 +1,10 @@
-package game_object_system.graphics_objects
+package game_engine.graphics
 
-class AnimationController {
+import com.badlogic.ashley.core.{Entity, Family}
+import com.badlogic.ashley.systems.IteratingSystem
+import game_object_system.{AnimationCom, RenderableCom}
 
+class AnimationController(priority : Int) extends IteratingSystem(
+  Family.all(classOf[RenderableCom], classOf[AnimationCom]).get(), priority){
+  override def processEntity(entity: Entity, deltaTime: Float): Unit = ???
 }
