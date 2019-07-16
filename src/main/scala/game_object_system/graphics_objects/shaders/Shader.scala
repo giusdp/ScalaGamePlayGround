a575cdd8548f556267e08df563cf0cdcffb57afb
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-16 20:42:03 +0200

    Added new type of shader and created a little interface to not repeat code

--- src/main/scala/game_object_system/graphics_objects/shaders/Shader.scala
+++ src/main/scala/game_object_system/graphics_objects/shaders/Shader.scala
@@ -4,8 +4,7 @@ import java.nio.FloatBuffer
 
 import org.lwjgl.opengl.GL20
 
-case class Shader(program : Int){
-
+case class Shader(program : Int) {
   val mvpLocation: Int = GL20.glGetUniformLocation(program, "mvp")
 
   def loadMVP(fb : FloatBuffer): Unit = GL20.glUniformMatrix4fv(mvpLocation, false, fb)
@@ -15,20 +14,25 @@ case class Shader(program : Int){
   def stop(): Unit = GL20.glUseProgram(0)
 
   def dispose(): Unit = GL20.glDeleteProgram(program)
-
 }
 
-case class TileMapShader(shader: Shader) {
+abstract class ShaderInterface(shader:Shader) {
+  def loadMVP(fb : FloatBuffer): Unit = shader.loadMVP(fb)
+  def use(): Unit = shader.use()
+  def stop(): Unit = shader.stop()
+  def dispose(): Unit = shader.dispose()
+}
+
+case class AnimatedSpriteShader(shader: Shader) extends ShaderInterface(shader) {
+  val spriteWidthLocation: Int = GL20.glGetUniformLocation(shader.program,"sprite_width")
+  val spriteHeightLocation: Int = GL20.glGetUniformLocation(shader.program, "sprite_height")
+  def loadSpriteWidth(tw : Float): Unit = GL20.glUniform1f(spriteWidthLocation, tw)
+  def loadSpriteHeight(th : Float): Unit = GL20.glUniform1f(spriteHeightLocation, th)
+}
+
+case class TileMapShader(shader: Shader) extends ShaderInterface(shader) {
   val tileWidthLocation: Int = GL20.glGetUniformLocation(shader.program, "tile_width")
   val tileHeightLocation: Int = GL20.glGetUniformLocation(shader.program, "tile_height")
-
-  def use(): Unit = shader.use()
-
-  def stop(): Unit = shader.stop()
-
-  def loadMVP(fb: FloatBuffer): Unit = shader.loadMVP(fb)
-
   def loadTextureTileWidth(tw : Float): Unit = GL20.glUniform1f(tileWidthLocation, tw)
   def loadTextureTileHeight(th : Float): Unit = GL20.glUniform1f(tileHeightLocation, th)
-
 }
