5c738c5e962e303fe210dd564c3b6040db4437c0
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-18 21:48:53 +0200

    handling case for animated sprite

--- src/main/scala/game_engine/graphics/SpriteRenderer.scala
+++ src/main/scala/game_engine/graphics/SpriteRenderer.scala
@@ -4,13 +4,13 @@ import java.util.Comparator
 
 import com.badlogic.ashley.core.{Entity, Family}
 import com.badlogic.ashley.systems.SortedIteratingSystem
-import game_object_system.graphics_objects.Camera
-import game_object_system.graphics_objects.shaders.Shader
+import game_object_system.graphics_objects.{AnimatedSprite, Camera, TextureAtlas}
+import game_object_system.graphics_objects.shaders.{AnimatedSpriteShader, Shader}
 import game_object_system.{ECEngine, PositionCom, RenderableCom}
 import org.lwjgl.BufferUtils
 import org.lwjgl.opengl.GL11
 
-class SpriteRenderer(shader : Shader, priority : Int) extends SortedIteratingSystem(
+class SpriteRenderer(shader : Shader, animShader : AnimatedSpriteShader, priority : Int) extends SortedIteratingSystem(
   Family.all(classOf[PositionCom], classOf[RenderableCom]).get(), Comp.comparator, priority) {
 
   val fb: FloatBuffer = BufferUtils.createFloatBuffer(16)
@@ -19,16 +19,38 @@ class SpriteRenderer(shader : Shader, priority : Int) extends SortedIteratingSys
 
     val sprite = ECEngine.renderableMapper.get(e).sprite
 
-    shader.use()
+
+
     sprite.model.bindModel()
     sprite.texture.bind(0)
 
     val mvp = Camera.getProjection.mulOrthoAffine(sprite.getModelMatrix)
-    shader.loadMVP(mvp.get(fb))
-    GL11.glDrawElements(GL11.GL_TRIANGLES, sprite.model.vCount, GL11.GL_UNSIGNED_INT, 0)
+
+
+    if (ECEngine.animMapper.has(e)) {
+      animShader.use()
+      shader.loadMVP(mvp.get(fb))
+
+      animShader.loadSpriteWidth(sprite.getWidth)
+      animShader.loadSpriteHeight(sprite.getHeight)
+//      animShader.
+      animShader.loadTexCoords(sprite.asInstanceOf[AnimatedSprite].frameCoords)
+
+      GL11.glDrawElements(GL11.GL_TRIANGLES, sprite.model.vCount, GL11.GL_UNSIGNED_INT, 0)
+
+      animShader.stop()
+    }
+    else {
+      shader.use()
+      animShader.loadMVP(mvp.get(fb))
+
+      GL11.glDrawElements(GL11.GL_TRIANGLES, sprite.model.vCount, GL11.GL_UNSIGNED_INT, 0)
+
+      shader.stop()
+    }
+
 
     sprite.model.unBindModel()
-    shader.stop()
   }
 
   def dispose(): Unit = {
