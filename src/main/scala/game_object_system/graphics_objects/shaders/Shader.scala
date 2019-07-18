16b4f4896270781739f4289ec8f8c4c6374443d1
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-18 21:47:43 +0200

    Added loading of tex coords

--- src/main/scala/game_object_system/graphics_objects/shaders/Shader.scala
+++ src/main/scala/game_object_system/graphics_objects/shaders/Shader.scala
@@ -26,8 +26,23 @@ abstract class ShaderInterface(shader:Shader) {
 case class AnimatedSpriteShader(shader: Shader) extends ShaderInterface(shader) {
   val spriteWidthLocation: Int = GL20.glGetUniformLocation(shader.program,"sprite_width")
   val spriteHeightLocation: Int = GL20.glGetUniformLocation(shader.program, "sprite_height")
+
   def loadSpriteWidth(tw : Float): Unit = GL20.glUniform1f(spriteWidthLocation, tw)
   def loadSpriteHeight(th : Float): Unit = GL20.glUniform1f(spriteHeightLocation, th)
+
+  def loadTexCoords(uvs : Array[Array[Float]]): Unit = loadTexCoords(uvs(0),uvs(1),uvs(2),uvs(3))
+
+  def loadTexCoords(tc0 : Array[Float], tc1 : Array[Float], tc2 : Array[Float], tc3 : Array[Float]): Unit = {
+    val loc0: Int = GL20.glGetUniformLocation(shader.program,"tex_offsets[0]")
+    val loc1: Int = GL20.glGetUniformLocation(shader.program,"tex_offsets[1]")
+    val loc2: Int = GL20.glGetUniformLocation(shader.program,"tex_offsets[2]")
+    val loc3: Int = GL20.glGetUniformLocation(shader.program,"tex_offsets[3]")
+
+    GL20.glUniform2fv(loc0, tc0)
+    GL20.glUniform2fv(loc1, tc1)
+    GL20.glUniform2fv(loc2, tc2)
+    GL20.glUniform2fv(loc3, tc3)
+  }
 }
 
 case class TileMapShader(shader: Shader) extends ShaderInterface(shader) {
