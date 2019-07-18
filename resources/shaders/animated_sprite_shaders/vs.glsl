2a4e2ca77dcf26f5509df85141c1fcd50c901fe3
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-18 20:54:29 +0200

    Added uniform to pass tex offsets for animated sprites

--- resources/shaders/animated_sprite_shaders/vs.glsl
+++ resources/shaders/animated_sprite_shaders/vs.glsl
@@ -1,17 +1,15 @@
 #version 330 core
 layout (location = 0) in vec3 pos;
-layout (location = 3) in vec2 tex_offset;
 
 uniform mat4 mvp;
 uniform float sprite_width;
 uniform float sprite_height;
 
+uniform vec2 tex_offsets[4];
+
 out vec2 tex_coords;
 
 void main(void){
     gl_Position = mvp * vec4(pos, 1.0);
-    if (gl_VertexID == 0){ tex_coords = tex_offset; }
-    else if (gl_VertexID == 1){ tex_coords = vec2(tex_offset.x, tex_offset.y + sprite_height); }
-    else if (gl_VertexID == 2){ tex_coords = vec2(tex_offset.x + sprite_width, tex_offset.y + sprite_height);}
-    else { tex_coords = vec2(tex_offset.x + sprite_width, tex_offset.y); }
+    tex_coords = tex_offsets[gl_VertexID];
 }
