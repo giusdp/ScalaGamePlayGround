837dd2fb7ae3190fdd0a635afc5f5f6a62dc4baf
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-07-19 14:10:57 +0200

    Added logic to handle frames

--- src/main/scala/game_engine/graphics/AnimationController.scala
+++ src/main/scala/game_engine/graphics/AnimationController.scala
@@ -2,9 +2,22 @@ package game_engine.graphics
 
 import com.badlogic.ashley.core.{Entity, Family}
 import com.badlogic.ashley.systems.IteratingSystem
-import game_object_system.{AnimationCom, RenderableCom}
+import game_object_system.graphics_objects.AnimatedSprite
+import game_object_system.{AnimationCom, ECEngine, RenderableCom}
 
 class AnimationController(priority : Int) extends IteratingSystem(
   Family.all(classOf[RenderableCom], classOf[AnimationCom]).get(), priority){
-  override def processEntity(entity: Entity, deltaTime: Float): Unit = ???
+
+  var elapsetTime : Float = 0
+
+  override def processEntity(entity: Entity, deltaTime: Float): Unit = {
+
+    val sprite = ECEngine.renderableMapper.get(entity).sprite.asInstanceOf[AnimatedSprite]
+
+    elapsetTime += deltaTime
+    if (elapsetTime >= sprite.fps) {
+      elapsetTime -= sprite.fps
+      sprite.nextFrame()
+    }
+  }
 }
